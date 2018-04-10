46275475a5cc070f853058a2eeb7b50db01a3442
YiGuo <>
2018-04-11 00:59:38 +0800

    上传
    1

---
+++ 放大缩小/放大缩小/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  放大缩小
+//
+//  Created by YiGuo on 2018/4/10.
+//  Copyright © 2018年 tbb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
